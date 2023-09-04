f7b263dad2aff54f20f89cf49c9065af11c131b0
amdiop-su <amdiop@syr.edu>
2023-09-04 14:57:16 -0400

    first commit

---
+++ starbucks/js/main.js
@@ -0,0 +1,27 @@
+// console.log("js has been loaded");
+
+// variables, methods and functios
+
+// 1. variables
+const myName = "Abdoulaye Diop";
+console.log(myName);
+
+const myAge = 18;
+const myCity = "New York";
+
+const introduction = `Hi, my name is ${myName}, I 
+am ${myAge} years old and I live in ${myCity}.`;
+
+console.log(introduction);
+
+
+// 2. methods
+const menuBtn = document.getElementById("menuBtn");
+const overlay = document.getElementById("overlay")
+console.log(menuBtn);
+
+// "event name", callback function
+menuBtn.addEventListener("click", function() {
+    //console.log("menu button has been clicked");
+    overlay.classList.add("active");
+}); // end of menuBtn click event
